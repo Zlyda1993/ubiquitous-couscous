76c1d2a2261e5e2d4e4bcf57bae6d1f95dee6089
Zachary Lyda <Oth1zlyda1993@gmail.com>
2023-09-12 15:20:20 -0400

    setting up general styles and core functionality

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 var startButton = document.querySelector("#start");
+var intro = document.querySelector(".intro")
+var quiz = document.querySelector(".quiz")
+quiz.style.display = "none"
+var questionEl = document.querySelector(".questionEl")
+var answersListEl = document.querySelector(".answersListEl")
 
 
 function questions() {
-    
+    intro.style.display = "none"
+    quiz.style.display = "block"
 }
-
-
 
 
 
