ac330f2ed08044d73465b375810debd769160e44
Zachary Lyda <Oth1zlyda1993@gmail.com>
2023-09-12 15:39:26 -0400

    etting up ability to shwo questions, and answers regarding that question, and adding styles to the answer buttons

--- script.js
+++ script.js
@@ -5,15 +5,43 @@ quiz.style.display = "none"
 var questionEl = document.querySelector(".questionEl")
 var answersListEl = document.querySelector(".answersListEl")
 
+var quizData = [
+    {
+        question: "What does JS stand for?",
+        answerSet: ["A. Just Sayin", "B. Java", "C. JavaScript", "D. Cascading Style Sheet"],
+        correct: "C. JavaScript"
+    },
+    {
+        question: "Which word is used for creating a variable in JavaScript?",
+        answerSet: ["A. for", "B. else", "C. function", "D. var"],
+        correct: "D. var"
+    },
+    {
+        question: 'What does JIT stand for, in "JIT compiler"?',
+        answerSet: ["A. Just In Time", "B. JavaScript Inverse Time", "C. Just Incase Try", "D. Java Is Tiny"],
+        correct: "A. Just In Time"
+    }
+]
 
-function questions() {
+var quizIndex = 0;
+
+
+function questions(quizIndex) {
     intro.style.display = "none"
     quiz.style.display = "block"
+    questionEl.innerHTML = quizData[quizIndex].question;
+    var answersForQuestion = quizData[quizIndex].answerSet
+    answersForQuestion.forEach(function(answerValue) {
+        var button = document.createElement("button")
+        button.innerHTML = answerValue;
+        answersListEl.append(button)
+    })
+
 }
 
 
 
 
-
-
-startButton.addEventListener("click", questions);
+startButton.addEventListener("click", function() {
+    questions(quizIndex)
+});
